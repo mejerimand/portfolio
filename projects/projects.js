86800cc00169f97727a09d1ace16e883063a94c1
mejerimand <326390272+mejerimand@users.noreply.github.com>
2026-09-16 12:52:59 +0200

    split ddc projects

--- projects/projects.js
+++ projects/projects.js
@@ -3,19 +3,19 @@ const projects = [
         id: "2023-speculative-tiktok",
         title: "Speculation on the Future of Tech, 2043",
         year: "2023 — Deep Dive into Speculative Design",
-        cover: "projects/2022-peech-internship/card-preview.png"
+        cover: "projects/2023-speculative-tiktok/speculative-design-newspaper.png"
+    },
+    { 
+        id: "2022-ddc-kl",
+        title: "Future of Welfare, 2050",
+        year: "2022 — Project Assistant at Danish Design Centre",
+        cover: "projects/2022-ddc-kl/KL-7.jpg"
     },
     {
         id: "2022-ddc-internship",
-        title: "Vorby - xxxsubtitle",
+        title: "Vorby",
         year: "2022–-2023 — Project Assistant at Danish Design Centre",
-        cover: "projects/2022-ddc-vorby/card-preview.png"
-    },
-    {
-        id: "2022-ddc-kl",
-        title: "DDC kl project???",
-        year: "2022 — Project Assistant at Danish Design Centre",
-        cover: "projects/2022-ddc-kl/card-preview.png"
+        cover: "projects/2022-ddc-vorby/vorby-2.jpg"
     },
     {
         id: "2022-bachelor-thesis",
