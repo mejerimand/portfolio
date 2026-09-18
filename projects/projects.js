653dcae68d50b57dd95237dd52aa872b10babe09
mejerimand <326390272+mejerimand@users.noreply.github.com>
2026-09-18 09:35:09 +0200

    ...

--- projects/projects.js
+++ projects/projects.js
@@ -2,25 +2,25 @@ const projects = [
     {
         id: "2025-fairies",
         title: "The Feral Cyborg Fairies",
-        year: "2025",
+        year: "2025 - Part of my Master's Thesis",
         cover: "projects/2025-fairies/fairies-1.jpg"
     },
     {
         id: "2025-witchwalk",
         title: "Witch Walks",
-        year: "2025",
+        year: "2025 - Part of my Master's Thesis",
         cover: "projects/2025-witchwalk/witchwalk-1.jpg"
     },
     {
         id: "2025-oceanspirits",
         title: "A Ritual for Living Together on a Damaged Planet",
-        year: "2025",
+        year: "2025 - Part of my Master's Thesis",
         cover: "projects/2025-oceanspirits/jellyfish-1.jpg"
     },
     {
         id: "2025-arcanedesigning",
         title: "Arcane Designing Cards - Prompts for the Imagination",
-        year: "2025 - ThingsCon",
+        year: "2025 - Part of my Master's Thesis, at ThingsCon 2025",
         cover: "projects/2025-arcanedesigning/cards-1.jpeg"
     },
     {
@@ -32,19 +32,19 @@ const projects = [
     { 
         id: "2022-ddc-kl",
         title: "Four Scenarios of Welfare in Denmark 2050",
-        year: "2022 — Danish Design Center",
+        year: "2023 — Danish Design Center",
         cover: "projects/2022-ddc-kl/KL_cover.jpg"
     },
     {
         id: "2023-speculative-tiktok",
         title: "Exploring the Future of Mobility, 2043",
-        year: "2023 — Part of the course Deep Dive into Speculative Design",
+        year: "2023 — Course: Deep Dive into Speculative Design",
         cover: "projects/2023-speculative-tiktok/speculative-design-newspaper_cover.jpg"
     },
     {
         id: "2024-itd",
         title: "DestinAItion - A Speculation on Regenerative Tourism",
-        year: "2024 — Part of the course Interactive Technology Design, with Hotelschool the Hauge as client",
+        year: "2024 — Course: Interactive Technology Design, with Hotelschool the Hauge as client",
         cover: "projects/2024-itd/itd-1.png"
     },
     {
